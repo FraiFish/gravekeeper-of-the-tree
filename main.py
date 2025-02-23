81158002aa3b972631456d17c8dd9648550b2186
FraiFish <trashell.cn@gmail.com>
2025-02-23 00:17:23 -0500

    renamed start.bat so i know what it is from the startup menu, also minimizes window

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from pathlib import Path
 import sys
 import time
 from audioplayer import AudioPlayer
+import ctypes
 FILEPATH = Path().absolute()
 
+# starts playing music, then minimizes window in 4 seconds, exits when song ends
 if __name__ == "__main__":
     playing = AudioPlayer(FILEPATH.__str__() + "\\music\\gravekeeper.mp3")
-    playing.volume = 75  # it was a bit too loud
     playing.play()
+    # print("\"ehh... *jams to music*\"")
     print("Now playing...")
-    input()
-    time.sleep(296)
+    time.sleep(4)
+    (ctypes.windll.user32
+     .ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6))
+    time.sleep(292)
     playing.close()
     sys.exit()
