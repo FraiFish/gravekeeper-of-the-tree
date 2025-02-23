26b6593245a9f58a6d25f29b3fd2e52638c89c81
FraiFish <trashell.cn@gmail.com>
2025-02-23 00:39:05 -0500

    fixed path in script

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ FILEPATH = Path().absolute()
 # waits then plays music, minimizes window in 4 seconds, exits when song ends
 if __name__ == "__main__":
     time.sleep(5)
-    playing = AudioPlayer(FILEPATH.__str__() + "\\music\\gravekeeper.mp3")
+    playing = AudioPlayer(FILEPATH.__str__() + "\\media\\gravekeeper.mp3")
     playing.play()
     # print("\"ehh... *jams to music*\"")
     print("Now playing...")
     time.sleep(4)
-    (ctypes.windll.user32
-     .ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6))
+    if "win" in sys.platform:
+        (ctypes.windll.user32
+            .ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6))
     time.sleep(292)
     playing.close()
     sys.exit()
